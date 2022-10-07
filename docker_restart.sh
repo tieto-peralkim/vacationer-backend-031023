6a6bd9f785b0a42c94d9ab1098e4b958e56172c9
Kimmo Perälä <kimmo.perala@tietoevry.com>
2022-10-07 16:21:33 +0300

    Testing setting env variables in docker run

--- docker_restart.sh
+++ docker_restart.sh
@@ -14,8 +14,7 @@ if [[ "$1" == "backend" ]] ; then
   envVariable1=(REACT_APP_ADDRESS="$2")
   envVariable2=(REACT_APP_MONGODB_URI="$3")
   envVariable3=(REACT_APP_SLACK_URI="$4")
-  echo "$envVariable1"
-  docker run -d -p $ports -e "${envVariable1[@]}" -e "${envVariable2[@]}" -e "${envVariable3[@]}" --name=vacationer-"$1" 629517020360.dkr.ecr.eu-west-1.amazonaws.com/ecr-vacationer-"$1":latest
+  docker run -d -p $ports -e "$envVariable1" -e "$envVariable2" -e "$envVariable3" --name=vacationer-"$1" 629517020360.dkr.ecr.eu-west-1.amazonaws.com/ecr-vacationer-"$1":latest
 else
   ports=80:80
   docker run -d -p $ports --name=vacationer-"$1" 629517020360.dkr.ecr.eu-west-1.amazonaws.com/ecr-vacationer-"$1":latest
