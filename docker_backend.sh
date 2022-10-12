f14da2a73f058966b20f1ca4fdaa1dfbe4724750
Kimmo Perälä <kimmo.perala@tietoevry.com>
2022-10-12 13:57:58 +0300

    Shell script bug

--- docker_backend.sh
+++ docker_backend.sh
@@ -13,6 +13,6 @@ PORTS=3001:3001
 ENV1=(REACT_APP_ADDRESS="$2")
 ENV2="REACT_APP_MONGODB_URI=mongodb://$3@docdb-sandbox.c9qzwnhx4tes.eu-west-1.docdb.amazonaws.com:27017/vacationer-app-database?tls=true&tlsCAFile=rds-combined-ca-bundle.pem&retryWrites=false"
 ENV3=(REACT_APP_SLACK_URI="$4")
-docker run -d -p $PORTS -e "$ENV1" -e "$ENV2" -e "$ENV3" --name=vacationer-"$INSTANCE" 629517020360.dkr.ecr.eu-west-1.amazonaws.com/ecr-vacationer-"$INSTANCE":latest
+docker run -d -p $PORTS -e "$ENV1" -e "$ENV2" -e "$ENV3" --name=vacationer-$INSTANCE 629517020360.dkr.ecr.eu-west-1.amazonaws.com/ecr-vacationer-"$INSTANCE":latest
 echo "Docker $INSTANCE run done, exiting EC2"
 exit
