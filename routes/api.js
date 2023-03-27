60132f551cfc0f7c05743a6cd10061ea2b435a16
Kimmo Perälä <kimmo.perala@tietoevry.com>
2023-03-27 13:11:17 +0300

    36 deleting manual swagger json

--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,6 @@ const timeframesRouter = require("./timeframes");
 const slackRouter = require("./slack");
 const middleWare = require("../utils/middleware");
 const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("../swagger.json");
 const swaggerJsdoc = require("swagger-jsdoc");
 const swaggerSchemas = require("../swaggerSchemas");
 
@@ -71,13 +70,12 @@ const swaggerSpec = swaggerJsdoc(options);
 // TODO: Define higher routes for other routers too
 module.exports = function (app) {
     app.use(express.json());
-    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-    // app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
     // loginRouter should NOT use checkAuthentication
     app.use("/", loginRouter);
 
     app.use(middleWare.checkAuthentication);
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
     app.use("/vacationers", vacationsRouter);
     app.use("/teams", teamsRouter);
     app.use("/", timeframesRouter);
