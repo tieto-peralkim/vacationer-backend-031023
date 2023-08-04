7af769e7ef9b78a631b0542dbc7d46de177af54a
Kimmo Perälä <kimmo.perala@tietoevry.com>
2023-08-04 16:40:19 +0300

    137 new logic for nager API and error handling

--- functions/slack.js
+++ functions/slack.js
@@ -3,10 +3,10 @@
 const fetcher = require("./fetcher.js");
 const handleVacationData = require("./handler");
 const axios = require("axios");
-let publicHolidays = [];
+let allPublicHolidays = [];
 
 const getNextWeekDates = () => {
-    let thisMonday = new Date(2023, 11, 30, 12);
+    let thisMonday = new Date(2023, 11, 3, 12);
     // thisMonday.setUTCDate(
     //     thisMonday.getUTCDate() - ((thisMonday.getUTCDay() + 6) % 7)
     // );
@@ -21,18 +21,19 @@ const getNextWeekDates = () => {
 
 const dayString = (day) => {
     let shortDateFormat = day[0].toISOString().substring(0, 10);
-    console.log("day", day[0].toISOString().substring(0, 10));
-    console.log("ph", typeof publicHolidays[0].date, typeof shortDateFormat);
 
-    let foundPublicHoliday = publicHolidays.find(
-        (o) => o.date === shortDateFormat
-    );
-    if (foundPublicHoliday) {
-        return `${foundPublicHoliday.localName}`;
+    // If nager.date API returned a value, check if date is public holiday
+    if (allPublicHolidays) {
+        let foundPublicHoliday = allPublicHolidays.find(
+            (o) => o.date === shortDateFormat
+        );
+        if (foundPublicHoliday) {
+            return `${foundPublicHoliday.localName}`;
+        }
     }
 
     if (day[1] === 0) {
-        return `${day[0].toLocaleDateString("fi-FI")}  ${day[1]}`;
+        return `${day[0].toLocaleDateString("fi-FI")}  0`;
     } else {
         // Normal holiday date structure: {date, amount of people on holiday, list of people on holiday}
         return `${day[0].toLocaleDateString("fi-FI")}  ${day[1]} - ${day[2]}`;
@@ -40,63 +41,59 @@ const dayString = (day) => {
 };
 
 async function getPublicHolidayData(year) {
-    try {
-        await axios
-            // Fetching Finnish public holidays from Public holiday API
-            .get(`https://date.nager.at/api/v3/publicholidays/${year}/FI`)
-            .then((response) => {
-                for (let i = 0; i < response.data.length; i++) {
-                    let publicDay = {};
-                    publicDay["date"] = response.data[i].date;
-                    publicDay["localName"] = response.data[i].localName;
-                    publicHolidays.push(publicDay);
-                }
-                console.log("publicHolidays", publicHolidays);
-            })
-            .catch((error) => {
-                console.error("There was a Public holiday API error!", error);
-            });
-    } catch (err) {
-        console.log(err);
+    let publicHolidays = [];
+
+    const response = await axios
+        // Fetching Finnish public holidays from Public holiday API
+        .get(`https://date.nager.at/api/v3/publicholidays/${year}/FIa`)
+        .catch(() => {
+            console.error("There was a Public holiday API error!");
+            return publicHolidays;
+        });
+
+    if (response.data) {
+        for (let i = 0; i < response.data.length; i++) {
+            let publicDay = {};
+            publicDay["date"] = response.data[i].date;
+            publicDay["localName"] = response.data[i].localName;
+            publicHolidays.push(publicDay);
+        }
+        return publicHolidays;
     }
 }
 
 // Create daily message for two work weeks (Monday[ma] to Friday[pe])
-function messageText(vacationers, days) {
-    const firstYear = days[0][0].getFullYear();
-    const nextYear = days[11][0].getFullYear();
+function sendFinalMessage(vacationers, days) {
+    let fullMessage = "";
 
-    getPublicHolidayData(firstYear).then(() =>
-        getPublicHolidayData(nextYear).then(() => {
-            return `🌴 *Tulevina viikkoina ${vacationers} lomalaista.*\n\n*Tällä viikolla:*\n>ma ${dayString(
-                days[0]
-            )}\n>ti ${dayString(days[1])}\n>ke ${dayString(
-                days[2]
-            )}\n>to ${dayString(days[3])}\n>pe ${dayString(
-                days[4]
-            )} \n*Ensi viikolla:*\n>ma ${dayString(days[7])}\n>ti ${dayString(
-                days[8]
-            )} \n>ke ${dayString(days[9])}\n>to ${dayString(
-                days[10]
-            )}\n>pe ${dayString(days[11])}`;
-        })
-    );
-}
-
-const slackMessageRequest = (vacationerAmount, weekList) => {
-    let finalMessage = "";
-    if (!vacationerAmount && !weekList) {
-        finalMessage = "🌴 *Ei lomalaisia seuraavana kahtena viikkona.*";
+    if (!vacationers && !days) {
+        fullMessage = "🌴 *Ei lomalaisia seuraavana kahtena viikkona.*";
     } else {
-        finalMessage = messageText(vacationerAmount, weekList);
+        fullMessage = `🌴 *Tulevina viikkoina ${vacationers} lomalaista.*\n\n*Tällä viikolla:*\n>ma ${dayString(
+            days[0]
+        )}\n>ti ${dayString(days[1])}\n>ke ${dayString(
+            days[2]
+        )}\n>to ${dayString(days[3])}\n>pe ${dayString(
+            days[4]
+        )} \n*Ensi viikolla:*\n>ma ${dayString(days[7])}\n>ti ${dayString(
+            days[8]
+        )} \n>ke ${dayString(days[9])}\n>to ${dayString(
+            days[10]
+        )}\n>pe ${dayString(days[11])}`;
+
+        if (!allPublicHolidays) {
+            fullMessage =
+                fullMessage + "\n\nHUOM! Julkisia lomapäiviä ei voitu hakea!";
+        }
     }
 
-    console.log("fm", finalMessage);
+    console.log("fm", fullMessage);
+
     // axios
     //     .post(
     //         process.env.REACT_APP_SLACK_URI,
     //         JSON.stringify({
-    //             text: finalMessage,
+    //             text: fullMessage,
     //         })
     //     )
     //     .then((response) => {
@@ -105,7 +102,45 @@ const slackMessageRequest = (vacationerAmount, weekList) => {
     //     .catch((error) => {
     //         console.error("There was a Slack post error!", error);
     //     });
-};
+}
+
+function slackMessageRequest(vacationerAmount, weekList) {
+    let nextWeekDates = getNextWeekDates();
+    let thisMonday = nextWeekDates.thisMonday;
+    let nextWeekFriday = nextWeekDates.nextWeekFriday;
+    const firstYear = thisMonday.getFullYear();
+    const nextYear = nextWeekFriday.getFullYear();
+
+    if (firstYear !== nextYear) {
+        getPublicHolidayData(firstYear)
+            .then((result) => {
+                allPublicHolidays = result;
+                getPublicHolidayData(nextYear)
+                    .then((result) => {
+                        if (result) {
+                            allPublicHolidays =
+                                allPublicHolidays.concat(result);
+                        }
+                        sendFinalMessage(vacationerAmount, weekList);
+                    })
+                    .catch((err) => {
+                        console.error(err);
+                    });
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    } else {
+        getPublicHolidayData(firstYear)
+            .then((result) => {
+                allPublicHolidays = result;
+                sendFinalMessage(vacationerAmount, weekList);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }
+}
 
 const sendSlackMessage = () => {
     let numberOfVacationers = 0;
